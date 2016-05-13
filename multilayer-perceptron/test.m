fee207546150afc67fd090010588b7b437509aba
mishuagopian <mishuagopian@gmail.com>
2016-05-13 18:10:26 -0300

    Adding incremental option for multilayer test

--- multilayer-perceptron/test.m
+++ multilayer-perceptron/test.m
@@ -1,18 +1,21 @@
 
-function ret = test(ft, g, g_der, n, b)
+% example of use: test(@xor, @exp_ft, @exp_ft_der, 0.5, 2, false)
+function ret = test(ft, g, g_der, n, b, incremental)
     inputs = [1 1; 1 0; 0 1; 0 0];
     s = ft(inputs);
 
     t{1} = inputs;
     t{2} = s;
 
-    n = 0.5;
-    b = 2;
     err = 0.001;
 
     nets = generate_nets([2 5 3 1]);
 
-    resolved_nets = multilayer_perceptron_batch(nets, t, err, g, g_der, n, b);
+    if (incremental)
+        resolved_nets = multilayer_perceptron_incremental(nets, t, err, g, g_der, n, b);
+    else
+        resolved_nets = multilayer_perceptron_batch(nets, t, err, g, g_der, n, b);
+    endif
 
     layer_outputs = forward_step(inputs, resolved_nets, g, b);
 
