fc9cf49c718f9094838f82abc3b3edef5484fb35
mishuagopian <mishuagopian@gmail.com>
2016-05-13 18:39:41 -0300

    Adding xor for 3 inputs in multilayer tests

--- multilayer-perceptron/test.m
+++ multilayer-perceptron/test.m
@@ -1,7 +1,8 @@
 
 % example of use: test(@xor, @exp_ft, @exp_ft_der, 0.5, 2, false)
 function ret = test(ft, g, g_der, n, b, incremental)
-    inputs = [1 1; 1 0; 0 1; 0 0];
+    inputs = [1 1 1; 1 0 1; 0 1 1; 0 0 1; 0 1 0; 1 0 0; 1 1 0; 0 0 0];
+    % inputs = [1 1; 1 0; 0 1; 0 0];
     s = ft(inputs);
 
     t{1} = inputs;
@@ -9,7 +10,7 @@ function ret = test(ft, g, g_der, n, b, incremental)
 
     err = 0.001;
 
-    nets = generate_nets([2 5 3 1]);
+    nets = generate_nets([3 5 2 1]);
 
     if (incremental)
         resolved_nets = multilayer_perceptron_incremental(nets, t, err, g, g_der, n, b);
