36ac6434e47911bf32190833e78d77902357b335
mishuagopian <mishuagopian@gmail.com>
2016-05-12 22:20:29 -0300

    xor

--- multilayer-perceptron/xor.m
+++ multilayer-perceptron/xor.m
@@ -1,8 +1,8 @@
 
-function ret = xor(inputs)
+function resp = xor(inputs)
     inputs_count = rows(inputs);
     for i = 1 : inputs_count
-        resp(i) = (sum(inputs(i, :)) == 1;
+        resp(i) = sum(inputs(i, :)) == 1;
     end
     resp = resp';
 end
