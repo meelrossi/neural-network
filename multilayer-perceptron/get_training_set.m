080cbaac13553ca72bdfd73df0191a7f91d2538a
mishuagopian <mishuagopian@gmail.com>
2016-05-19 21:52:38 -0300

    Getting 1 of 4 and outers

--- multilayer-perceptron/get_training_set.m
+++ multilayer-perceptron/get_training_set.m
@@ -7,12 +7,29 @@ function ret = get_training_set(data_filename)
     patterns_count = rows(dataset_sorted);
     delete_count = 0;
 
+    min_x = min(dataset_sorted(:, 1));
+    max_x = max(dataset_sorted(:, 1));
+    min_y = min(dataset_sorted(:, 2));
+    max_y = max(dataset_sorted(:, 2));
+
     for i = 1 : patterns_count
-        if (mod(i, 2) != 0)
+
+        x = dataset_sorted(i - delete_count, 1);
+        y = dataset_sorted(i - delete_count, 2);
+
+        is_outer_bool = is_outer(min_x, max_x, min_y, max_y, x, y);
+
+        if (mod(i, 4) != 0 && !is_outer_bool)
         	dataset_sorted(i - delete_count, :) = [];
         	delete_count++;
         endif
+
     end
 
+    delete_count
 	ret = dataset_sorted;
+end
+
+function bool = is_outer(min_x, max_x, min_y, max_y, x, y)
+    bool = (min_x == x && min_y == y) || (min_x == x && max_y == y) || (max_x == x && min_y == y) || (max_x == x && max_y == y);
 end
